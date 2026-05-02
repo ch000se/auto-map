3a37faee5b61ed8db3eea573bb5566d4d747e9c3
ch000se <svyatikchaykivskiy@gmail.com>
2026-05-02 15:10:48 +0300

    Load gradle-public.properties so CI sees POM metadata

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,6 +30,18 @@ dependencyResolutionManagement {
 
 rootProject.name = "automap"
 
+val publicProps = java.util.Properties().apply {
+    val file = rootDir.resolve("gradle-public.properties")
+    if (file.exists()) file.inputStream().use { load(it) }
+}
+gradle.beforeProject {
+    publicProps.forEach { (k, v) ->
+        if (!project.hasProperty(k.toString())) {
+            project.extensions.extraProperties[k.toString()] = v.toString()
+        }
+    }
+}
+
 include(":lib-core")
 include(":lib-compiler")
 include(":app-example")
